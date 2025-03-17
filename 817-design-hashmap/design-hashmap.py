798184461e0330de53f95265076101a4289f2866
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-03-16 18:43:03 -0700

    Time: 68 ms (32.24%) | Memory: 22.2 MB (27.33%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,26 +1,23 @@
 class MyHashMap:
-
     def __init__(self):
-        self.hash_map = {}
-        
+        self.n = 10000
+        self.arr = [[] for _ in range(self.n)]
 
     def put(self, key: int, value: int) -> None:
-        self.hash_map[key] = value
+        idx = key % self.n
+        for i, (k, v) in enumerate(self.arr[idx]):
+            if k == key:
+                self.arr[idx][i] = (key, value)
+                return
+        self.arr[idx].append((key, value))
 
     def get(self, key: int) -> int:
-        res = -1
-        if key in self.hash_map.keys():
-            res = self.hash_map[key]
-        return res
-        
+        idx = key % self.n
+        for k, v in self.arr[idx]:
+            if k == key:
+                return v
+        return -1
 
     def remove(self, key: int) -> None:
-        if key in self.hash_map.keys():
-            del self.hash_map[key] 
-
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+        idx = key % self.n
+        self.arr[idx] = [(k, v) for k, v in self.arr[idx] if k != key]
