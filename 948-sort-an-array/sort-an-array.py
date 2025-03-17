102799b88eee74d0292b26988d41a75dc9aa83b0
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-03-17 13:21:07 -0700

    Time: 742 ms (23.72%) | Memory: 24.3 MB (79.11%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,31 +1,34 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        if len(nums) > 1: 
+        if len(nums) > 1:
             mid = len(nums)//2
-            L = nums[:mid] 
-            R = nums[mid:] 
+            L = nums[:mid]
+            R = nums[mid:]
 
             self.sortArray(L)
             self.sortArray(R)
-
+            # print(f"L is {L}")
+            # print(f"R is {R}")
             i = j = k = 0
+            while i < len(L) and j < len(R):
+                # print(f"i: {i}, j: {j}, k: {k}")
 
-            while i < len(L) and j < len(R): 
-                if L[i] < R[j]: 
-                    nums[k] = L[i] 
-                    i+=1
-                else: 
-                    nums[k] = R[j] 
-                    j+=1
-                k+=1
-
-            while i < len(L): 
-                nums[k] = L[i] 
-                i+=1
-                k+=1
-
-            while j < len(R): 
-                nums[k] = R[j] 
-                j+=1
-                k+=1
+                if L[i] < R[j]:
+                    nums[k] = L[i]
+                    i += 1
+                else:
+                    nums[k] = R[j]
+                    j += 1
+                k += 1
+            
+            while i < len(L):
+                nums[k] = L[i]
+                i += 1
+                k += 1
+            
+            while j < len(R):
+                nums[k] = R[j]
+                j += 1
+                k += 1
         return nums
+                
