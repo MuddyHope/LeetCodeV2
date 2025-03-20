7b6f46c4d307c7ceed38efdec0bf74efec394478
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-03-20 00:11:26 -0700

    Time: 105 ms (71.79%) | Memory: 31.1 MB (33.23%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,13 +1,21 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.dp=[[0] * (len(matrix[0])+1) for _ in range(len(matrix)+1)]
-        
-		# calculate prefix sum
-        for r in range(len(self.dp)-1):
-            for c in range(len(self.dp[0])-1):
-                self.dp[r+1][c+1]=matrix[r][c] + self.dp[r][c+1] + self.dp[r+1][c] - self.dp[r][c]
+        rows = len(matrix)
+        cols = len(matrix[0])
+        self.sum_mat = [[0] * (cols + 1) for r in range(rows+1)]
+
+        for each_row in range(rows):
+            prefix_sum = 0
+            for each_col in range(cols):
+                prefix_sum += matrix[each_row][each_col]
+                above = self.sum_mat[each_row][each_col + 1]
+                self.sum_mat[each_row+1][each_col+1] = prefix_sum + above
+            # print(self.sum_mat)
         
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.dp[row2+1][col2+1] - self.dp[row1][col2+1] - self.dp[row2+1][col1] + self.dp[row1][col1]
-                
+        bottom_right = self.sum_mat[row2+1][col2+1]
+        left = self.sum_mat[row2+1][col1]
+        above = self.sum_mat[row1][col2+1]
+        top_left = self.sum_mat[row1][col1]
+        return bottom_right - left - above + top_left
