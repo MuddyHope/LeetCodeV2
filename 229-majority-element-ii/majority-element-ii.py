0b307bd4b0c121f395ed8d985148dfdc0ae2d4bd
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-03-25 22:55:51 -0700

    Time: 7 ms (31.95%) | Memory: 19.1 MB (78.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -2,13 +2,13 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         # brute force method
         max_count = len(nums)
-        res = set()
+        res = []
         # use hash-map and list
         hash_map = {}
         for each_num in nums:
             hash_map[each_num] = hash_map.get(each_num, 0) + 1
-            if hash_map[each_num] > max_count//3:
-                    res.add(each_num)
-        return list(res)
+            if hash_map[each_num] > max_count//3 and each_num not in res:
+                    res.append(each_num)
+        return res
 
         
