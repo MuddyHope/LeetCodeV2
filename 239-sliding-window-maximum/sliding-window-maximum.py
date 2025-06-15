f45a86cd7df94a88d1279ff01f3198077e2eb9a2
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-06-15 12:13:28 -0700

    Time: 171 ms (54.26%) | Memory: 31 MB (86.76%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -2,22 +2,23 @@ from collections import deque
 from typing import List
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        dq = deque()  # stores indices of nums
+        output = []
 
-        for i in range(len(nums)):
-            # Remove indices out of window
-            if dq and dq[0] < i - k + 1:
+        left = right = 0
+        dq = deque()
+
+        while right < len(nums):
+            # check deque if the dq[0] is smaller than current number
+            while dq and nums[dq[-1]] < nums[right]:
+                dq.pop()
+            dq.append(right)
+
+            if left > dq[0]:
                 dq.popleft()
 
-            # Remove smaller values from the back
-            while dq and nums[dq[-1]] < nums[i]:
-                dq.pop()
+            if right + 1 >= k:
+                output.append(nums[dq[0]])
+                left += 1
 
-            dq.append(i)
-
-            # Add to result once window is of size k
-            if i >= k - 1:
-                res.append(nums[dq[0]])
-
-        return res
+            right += 1
+        return output
