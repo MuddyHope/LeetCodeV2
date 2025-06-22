d8732e83e5136eccd52d2ed613f6273e5cc4b8c5
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-06-22 16:28:37 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.99%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,27 +1,24 @@
-from typing import Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(
-        self, list1: Optional[ListNode], list2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        dummy = ListNode()  # dummy node to simplify head management
-        tail = dummy  # tail is the current end of the merged list
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # make a dummy ListNode
+        new_list = ListNode()
+        # take a copy of the node
+        head = new_list
 
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1
+                head.next = list1
                 list1 = list1.next
             else:
-                tail.next = list2
+                head.next = list2
                 list2 = list2.next
-            tail = tail.next  # advance tail
+            head = head.next
+        
+        head.next = list1 if list1 else list2
 
-        # Attach any remaining nodes
-        tail.next = list1 if list1 else list2
-
-        return dummy.next  # skip dummy and return real head
+        return new_list.next
