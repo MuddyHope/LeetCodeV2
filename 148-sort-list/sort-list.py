145f8cdaa1957a19cd4123a6f6de2688c93d6674
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-06-23 12:27:30 -0700

    Time: 75 ms (78.15%) | Memory: 37.9 MB (5.78%) - LeetSync

---
+++ 148-sort-list/sort-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        # brute force method
+        vals = []
+        while curr:
+            vals.append(curr.val)
+            curr = curr.next
+        
+        vals = sorted(vals)
+        print(vals)
+        dummy = new_head = ListNode()
+
+        for each in vals:
+            new_node = ListNode(each)
+            new_head.next = new_node  # Link it
+            new_head = new_node       # Move the pointer forward
+
+        return dummy.next
