bbdd69fc767ab3972f6b1bd73202a5564e336d62
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-06-25 01:31:22 -0700

    Time: 2 ms (53.50%) | Memory: 23.4 MB (11.90%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -10,25 +10,36 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         # find mid
-        slow , fast =  head, head.next
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # mid-point is in second.next
-        # now we have to reverse the second half
-        second = slow.next
-        prev = slow.next = None
+        # mid is slow.next
+
+        # now reverse the second half
+        second = slow.next  # 3
+        prev = slow.next = None # None
         while second:
-            temp = second.next
-            second.next = prev
-            prev = second
-            second = temp
-        
-        # now merging it
+            temp = second.next # 4 # 5
+            second.next = prev  # 3 -> None # 4 -> 3 -> None
+            prev = second # 3   # 4
+            second = temp   # 4 # 5
+        # print_list(prev)
+        # [5] -> [4] -> None
+
+        # print_list(head)
+        # [1] -> [2] -> [3] -> None
+
+        # now we have to merge it
         first, second = head, prev
         while second:
-            temp1, temp2 = first.next, second.next
-            first.next = second
-            second.next = temp1
-            first, second = temp1, temp2
+            temp1, temp2 = first.next, second.next  # 2, 4 # None, 3
+            first.next = second # 1->5  # 2->4
+            second.next = temp1 # 1->5->2   #4->None
+            first, second  = temp1, temp2   # 2, 4 # None, 3
+
+
+
+
+
