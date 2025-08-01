2dfe58bdd30e06ee95770560e13cbf2b3626bd1a
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-08-01 13:23:56 -0700

    Time: 100 ms (99.92%) | Memory: 18.9 MB (83.73%) - LeetSync

---
+++ 690-employee-importance/employee-importance.py
@@ -0,0 +1,34 @@
+from collections import deque
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        employees_map = {}
+        for each in employees:
+            employees_map[each.id] = each
+    
+        return self.recursive(employees_map, id)
+
+
+        
+    def recursive(self, employee_map, id):
+        
+        if not employee_map.get(id):
+            return 0
+        
+        _idx = employee_map.get(id)
+        total_val = _idx.importance
+
+        for each_sub in _idx.subordinates:
+            total_val += self.recursive(employee_map, each_sub)
+
+        return total_val
+        
+            
