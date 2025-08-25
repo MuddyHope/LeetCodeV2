93ed5d8859b8fda883a3e11de9a690886623008d
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-08-25 15:40:35 -0700

    Time: 35 ms (10.97%) | Memory: 18 MB (14.54%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,44 +1,24 @@
-from typing import List
-
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         candidates.sort()
-
         subset = []
-        
-        def dfs(i):
-            # Base Case 1: Target is met
-            if sum(subset) == target:
+
+        def dfs(i, subset, total):
+            if total == target:
                 res.append(subset.copy())
                 return
-            
-            # Base Case 2: Target is exceeded or we are out of candidates
-            if sum(subset) > target or i >= len(candidates):
+            if i == len(candidates) or total > target:
                 return
-            
-            # --- Decision Point: Take the current element ---
-            
-            # Add the current candidate to the subset
             subset.append(candidates[i])
-            
-            # Recursively call with the next index (i + 1)
-            dfs(i + 1)
-            
-            # --- Backtrack: Undo the decision to explore another path ---
-            
-            # Remove the last element added
+            # adding next value
+            dfs(i+1, subset, total+candidates[i])
+
             subset.pop()
-            
-            # --- Decision Point: Skip all duplicate elements ---
-            
-            # This is the key to handling duplicates. We skip any
-            # consecutive elements that are the same as the one we just processed.
             while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
                 i += 1
-            
-            # Recursively call to explore combinations *without* the current element
-            dfs(i + 1)
-
-        dfs(0)
+            dfs(i+1, subset, total)
+        dfs(0, subset, 0)
+        # print(res)
         return res
+            
