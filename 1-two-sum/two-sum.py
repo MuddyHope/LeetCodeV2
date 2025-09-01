66d1f7c72b6e51ee6c5a8069caaa673894ba99aa
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-09-01 16:56:02 -0700

    Time: 2 ms (53.42%) | Memory: 19 MB (23.37%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,13 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Your code here
         my_dict = {}
-        
-        for each in range(len(nums)):
-            if target - nums[each] in my_dict.keys():
-                return [each, my_dict[target-nums[each]]]
+        for i in range(len(nums)):
+            if target - nums[i] in my_dict:
+                return [i, my_dict[target-nums[i]]]
             else:
-                my_dict[nums[each]] = each
-        return []
+                my_dict[nums[i]] = i
+        print(my_dict)
+    
