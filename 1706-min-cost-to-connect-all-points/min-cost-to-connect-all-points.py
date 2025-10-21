6c2302c92be4c58b5ecb1eeddb863f51e2319499
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-10-20 20:03:51 -0700

    Time: 1892 ms (38.97%) | Memory: 196.3 MB (6.43%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,30 +1,33 @@
+from collections import defaultdict
+import heapq
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
 
-        # iterate over each points, by starting at some point
+        # lets go through with prims algorithm
+
         N = len(points)
-        adj = {i: [] for i in range(N)}
+        adj = defaultdict(list)
+
         for i in range(N):
             x1, y1 = points[i]
-            for j in range(i + 1, N):
+            for j in range(i+1, N):
                 x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                adj[i].append([dist, j])
-                adj[j].append([dist, i])
-        # print(adj)
+                dist = abs(x1-x2) + abs(y1-y2)
+                adj[i].append((dist, j))
+                adj[j].append((dist, i))
 
 
+        minHeap = [(0,0)]
+        visited = set()
         res = 0
-        visit = set()
-        minH = [[0, 0]]
-        while len(visit) < N:
-            cost, i = heapq.heappop(minH)
-            if i in visit:
+        while len(visited) < N:
+            dist, i = heapq.heappop(minHeap)
+            if i in visited:
                 continue
-            res += cost
-            visit.add(i)
-            for neiCost, nei in adj[i]:
-                if nei not in visit:
-                    heapq.heappush(minH, [neiCost, nei])
+            visited.add(i)
+            res += dist
+            for nei_dist, nei in adj[i]:
+                heapq.heappush(minHeap, (nei_dist, nei))
+        
         return res
-
