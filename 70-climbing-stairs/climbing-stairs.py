64429f769d2d8a8cfed0563fea4f19a3e1a30fe9
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-10-23 01:35:41 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.77%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,7 @@
+from functools import lru_cache
+class Solution:
+    @lru_cache
+    def climbStairs(self, n: int) -> int:
+        if n == 1 or n == 0:
+            return 1
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
