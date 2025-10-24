69677388a8a51add076ec19fc39508897cc7ac6f
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-10-23 17:13:02 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.35%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,7 +3,5 @@ class Solution:
         nums.extend([0,0])
 
         for i in range(len(nums) - 3, -1, -1):
-            print(i)
             nums[i] = max(nums[i] + nums[i+2], nums[i+1])
-            print(nums)
         return nums[0]     
