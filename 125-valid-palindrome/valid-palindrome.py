4d3feba7790a4ef2a0a92d721b78cb99aff8de4e
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-11-14 14:28:32 -0800

    Time: 15 ms (12.63%) | Memory: 18.1 MB (55.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,16 +4,11 @@ class Solution:
 
         left, right = 0, len(s) - 1
 
-        print(ord('0'), ord('9'))
-
         while left <= right:
-            print(f"currently checking for : {s[left]}, {s[right]}")
             if not 97 <= ord(s[left].lower()) <= 122 and not 48 <= ord(s[left].lower()) <= 57:
-                print(f"left: {s[left]}")
                 left += 1
                 continue
             if not 97 <= ord(s[right].lower()) <= 122 and not 48 <= ord(s[right].lower()) <= 57:
-                print(f"right : {s[right]}")
                 right -= 1
                 continue
 
