57070755124db8299fc7092caaf11203e9327527
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2025-12-01 14:21:54 -0800

    Time: 23 ms (5.07%) | Memory: 19.1 MB (51.37%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -0,0 +1,37 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        stack = []
+
+        for each in tokens:
+            print(f"current op: {each}, type: {type(each)}")
+
+            # check number
+            try:
+                num = int(each)
+                stack.append(num)
+                # print(stack)
+                continue
+            except:
+                pass
+
+            if each == "+":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b + a)
+            elif each == "-":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b - a)
+            elif each == "*":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b * a)
+            elif each == "/":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b / a))   # truncate toward zero
+
+            # print(stack)
+
+        return stack.pop()
