03c80fe878a297a61bc1688914bd49f48389f91c
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-03-12 15:11:57 -0700

    Time: 539 ms (7.77%) | Memory: 19.7 MB (91.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,46 +1,48 @@
 from collections import Counter
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        # t is the slider per se.
-        # s is the the final
 
-        # first approach
-        '''
-        Have a set that has all the available substring, loop over the set and take the least number of 
-        '''
-        '''
-        Now for it loop over s with size t, break out of the loop once you have a match
-        '''
+class Solution:
+    def minWindow(self, s: str, t: str):
+
         res = ""
-        t_count = Counter(t)
-        window_count = {}
+        min_length = float("inf")
+
+        if len(t) > len(s):
+            return res
+
+        t_dict = Counter(t)
+        s_dict = {}
 
         left = 0
-        min_len = float('inf')
-        min_window = ""
 
         for right in range(len(s)):
-            # Add current character to window
-            window_count[s[right]] = window_count.get(s[right], 0) + 1
 
-            # While window contains all chars in t, try shrinking it
-            while all(window_count.get(ch, 0) >= count for ch, count in t_count.items()):
-                current_window_len = right - left + 1
-                if current_window_len < min_len:
-                    min_len = current_window_len
-                    min_window = s[left:right+1]
+            s_dict[s[right]] = 1 + s_dict.get(s[right], 0)
 
-                # Remove the left char and shrink window
-                window_count[s[left]] -= 1
-                if window_count[s[left]] == 0:
-                    del window_count[s[left]]
+            # check if window satisfies t
+            valid = True
+            for c in t_dict:
+                if s_dict.get(c, 0) < t_dict[c]:
+                    valid = False
+                    break
+
+            while valid:
+
+                if (right - left + 1) < min_length:
+                    min_length = right - left + 1
+                    res = s[left:right+1]
+
+                # shrink window
+                s_dict[s[left]] -= 1
+                if s_dict[s[left]] == 0:
+                    del s_dict[s[left]]
+
                 left += 1
-        return min_window
 
+                # check validity again
+                valid = True
+                for c in t_dict:
+                    if s_dict.get(c, 0) < t_dict[c]:
+                        valid = False
+                        break
 
-
-
-        
-
-
-        
+        return res
