d0743c85e544fd78a2d1ade89f9e54bef02a5fd0
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-03-17 20:39:47 -0700

    Time: 0 ms (100.00%) | Memory: 22.6 MB (39.89%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+        self.min_stack = deque()   # tracks min at each level
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(val, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
