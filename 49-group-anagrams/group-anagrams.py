58315f43021098e3c5d2de0917188151d460f018
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-11 13:45:02 +0530

    Time: 15 ms (44.09%) | Memory: 24.1 MB (9.74%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,10 @@
-from collections import Counter, defaultdict
-from typing import List
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash_map = {}
+        res = defaultdict(list)
+        for words in strs:
+            count = [0] * 26
 
-        for word in strs:
-            key = tuple(sorted(Counter(word).items()))
-
-            if key in hash_map:
-                hash_map[key].append(word)
-            else:
-                hash_map[key] = [word]
-
-        return list(hash_map.values())
+            for ch in words:
+                count[ord(ch) - ord('a')] += 1
+            res[tuple(count)].append(words)
+        return list(res.values())
