d1200dddb9aaaa3f8adaf86b4abb592b85179673
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-11 14:33:22 +0530

    Time: 7 ms (52.19%) | Memory: 22.7 MB (94.90%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        my_dict = Counter(nums)
         
-        my_res = sorted(my_dict.keys(), key=lambda x: my_dict[x], reverse=True)[:k]
-        return my_res
+        # run through the list, make a dictionary, sort them based off of values, convert them into a list and show [:k]
+
+        hash_map = {}
+
+        for i in nums:
+            hash_map[i] = hash_map.get(i, 0) + 1
+
+        new_list = sorted(hash_map.keys(), key=lambda x: hash_map[x], reverse=True)
+        return new_list[:k]
