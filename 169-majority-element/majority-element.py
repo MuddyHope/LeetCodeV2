8ad67cff3610f150191c19eccde7d7d5e7cfe5c4
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-12 17:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (98.48%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        # bucket sort
 
-        buckets = [[] for _ in range(len(nums) + 1)]
-        counter = Counter(nums)
+        # Boyer - Moore Algorithm
 
-        for num, count in counter.items():
-            buckets[count].append(num)
-        
-        # print(buckets)
+        candidate = 0
+        count = 0
 
-        # loop in reverse
-        for num in reversed(range(len(buckets))):
-            if buckets[num]:
-                return buckets[num][0]
+        for each in nums:
+            if count == 0:
+                candidate = each
+            
+            if each == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
