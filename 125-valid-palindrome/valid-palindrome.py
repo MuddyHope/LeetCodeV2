9da3335808d69970dd26401fe70f3b8b858a2f59
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-12 17:01:25 +0530

    Time: 7 ms (81.18%) | Memory: 19.5 MB (78.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        # go with two-pointer
-        # a: 97, z: 122
-        left, right = 0, len(s) - 1
-        while left <= right:
-            if not s[left].isalnum():
-                left += 1
-            elif not s[right].isalnum():
-                right -= 1
+        i, j = 0, len(s) - 1
 
-            elif s[left].lower() == s[right].lower() :
+        while i < j:
 
-                left += 1
-                right -= 1
-            else:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            elif not s[i].lower() == s[j].lower():
                 return False
+            i += 1
+            j -= 1
         return True
-
-        
+            
