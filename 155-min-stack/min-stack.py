f8f8b097248562fdde0262f3853f67072a006260
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-15 16:21:07 +0530

    Time: 63 ms (26.95%) | Memory: 32.9 MB (5.24%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,24 +1,33 @@
-from collections import deque
-
 class MinStack:
 
     def __init__(self):
-        self.stack = deque()
-        self.min_stack = deque()   # tracks min at each level
+        self.stack = []
+        self.min_stack = [float(inf)]
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.min_stack:
-            self.min_stack.append(val)
-        else:
-            self.min_stack.append(min(val, self.min_stack[-1]))
+    def push(self, value: int) -> None:
+        # print(f"stack: {self.stack}, min_stack: {self.min_stack}")
+        self.stack.append(value)
+        if self.min_stack[-1] >= value:
+            self.min_stack.append(value)
 
     def pop(self) -> None:
-        self.stack.pop()
-        self.min_stack.pop()
+        # print(f"stack: {self.stack}, min_stack: {self.min_stack}")
+        val = self.stack.pop()
+        if self.min_stack[-1] == val:
+            self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
-
+        
     def getMin(self) -> int:
         return self.min_stack[-1]
+
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
