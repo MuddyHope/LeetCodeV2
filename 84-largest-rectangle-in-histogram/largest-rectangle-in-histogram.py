9d1ef4cf30119974b47307fd7446328ce0ca47c3
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-16 00:43:26 +0530

    Time: 123 ms (51.05%) | Memory: 38.3 MB (5.64%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,20 +1,20 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        heights.append(0)  # Sentinel to flush out stack
-        stack = []  # (index, height)
-        max_area = 0
+        
+        # add the minimum of curr_height and the stack[-1] height to the stack
+        # if the height of the next height is smaller than the top of the stack height, pop it
 
-        for i, h in enumerate(heights):
-            while stack and h < stack[-1][1]:
-                index, height = stack.pop()
-                # Width is from current i to previous bar in stack
-                width = i if not stack else i - stack[-1][0] - 1
-                curr_area = height * width
-                # print(f"Pop: index={index}, height={height}")
-                # print(f" -> Width: {width}, Area: {curr_area}")
-                max_area = max(max_area, curr_area)
-                # print(f" -> Max Area: {max_area}")
-            stack.append((i, h))
-            # print(f"Push: stack={stack}")
-        # print(f"max-area", max_area)
+        stack = []  # (idx, height)
+        max_area = 0
+        for i in range(len(heights)):
+            start = i
+            while stack and stack[-1][1] > heights[i]:
+                idx, height = stack.pop()
+                max_area = max(max_area, height * (i - idx))
+                start = idx
+            stack.append([start, heights[i]])
+        
+
+        for i, h in stack:
+            max_area = max(max_area, h * (len(heights) - i))
         return max_area
