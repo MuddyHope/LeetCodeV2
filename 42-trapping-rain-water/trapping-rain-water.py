3a59469a686e17622d98c3a54f4712f291d322f7
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-15 21:13:15 +0530

    Time: 12 ms (32.71%) | Memory: 21 MB (48.98%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,31 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
 
-        # Edge case
-        if n == 0:
-            return 0
+        left_max = [0] * len(height)
+        right_max = [0] * len(height)
 
-        # LEFT → RIGHT max
-        left_2_right = []
-        curr_max = 0
-        for h in height:
-            curr_max = max(curr_max, h)
-            left_2_right.append(curr_max)
-        print(left_2_right)
+        # left max
+        left_max[0] = height[0]
+        for i in range(1, len(height)):
+            left_max[i] = max(left_max[i - 1], height[i])
 
-        # RIGHT → LEFT max
-        right_2_left = []
-        curr_max = 0
-        for h in height[::-1]:
-            curr_max = max(curr_max, h)
-            right_2_left.append(curr_max)
-        right_2_left = right_2_left[::-1]
-        print(right_2_left)
+        # right max
+        right_max[-1] = height[-1]
+        for i in range(len(height) - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], height[i])
 
-        # Compute trapped water
-        water = 0
-        for i in range(n):
-            water += min(left_2_right[i], right_2_left[i]) - height[i]
-
-        return water
+        
+        res = 0
+        for i in range(len(height)):
+            res += (min(left_max[i], right_max[i]) - height[i])
+        return res
