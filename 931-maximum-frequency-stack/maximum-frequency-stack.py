2fba112163a583d1e95dc9d80f77ffad05609e70
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-16 12:43:13 +0530

    Time: 72 ms (68.20%) | Memory: 27.8 MB (61.29%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -1,29 +1,32 @@
-from collections import defaultdict
-
 class FreqStack:
 
     def __init__(self):
-        self.freq = {}                     # val -> frequency
-        self.group = defaultdict(list)     # frequency -> stack of values
+        self.hash = {}  # val, count
+        self.group = defaultdict(list) # freq, [vals]
         self.max_freq = 0
 
     def push(self, val: int) -> None:
+        self.hash[val] = 1 + self.hash.get(val, 0)
 
-        f = 1 + self.freq.get(val, 0)
-        self.freq[val] = f
+        _g = self.hash[val]
+        if _g > self.max_freq:
+            self.max_freq = _g
+        
+        self.group[_g].append(val)
 
-        if f > self.max_freq:
-            self.max_freq = f
-
-        self.group[f].append(val)
+        
 
     def pop(self) -> int:
-
         val = self.group[self.max_freq].pop()
 
-        self.freq[val] -= 1
+        self.hash[val] -=1
 
         if not self.group[self.max_freq]:
             self.max_freq -= 1
 
         return val
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
