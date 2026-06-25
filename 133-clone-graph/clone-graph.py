3cc5ef420c6c4a77ac444317bc4f4c36894a4f8d
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-25 12:42:43 +0530

    Time: 49 ms (61.53%) | Memory: 19.8 MB (53.31%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,32 +1,25 @@
-from typing import Optional
-import collections
-
+"""
 # Definition for a Node.
 class Node:
-    def __init__(self, val=0, neighbors=None):
+    def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
+"""
 
+from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return None
+        
 
-        # Map original node -> cloned node
-        clone_map = {node: Node(node.val)}
+        oldToNew = {}
 
-        dq = collections.deque([node])
-
-        while dq:
-            curr = dq.popleft()
-
-            for nei in curr.neighbors:
-                if nei not in clone_map:
-                    # Clone the neighbor
-                    clone_map[nei] = Node(nei.val)
-                    dq.append(nei)
-                
-                # Append the cloned neighbor to the cloned node’s neighbors
-                clone_map[curr].neighbors.append(clone_map[nei])
-
-        return clone_map[node]
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        return dfs(node) if node else None
