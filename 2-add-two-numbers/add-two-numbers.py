31329032f82f2c62b8bcb21c83de8033ffced7f5
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-06-30 23:32:49 +0530

    Time: 6 ms (28.34%) | Memory: 19.3 MB (78.10%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,36 +1,22 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        # traverse from head
-        head_1, head_2 = l1, l2
+    def addTwoNumbers(self, l1, l2):
         dummy = ListNode(0)
         curr = dummy
-
         carry = 0
-        while head_1 or head_2 or carry:
-            v1 = head_1.val if head_1 else 0
-            v2 = head_2.val if head_2 else 0
-            curr_sum = carry + v1 + v2
-            # create new node
-            carry = curr_sum // 10
-            curr_sum = curr_sum % 10 
 
-            new_node = ListNode(curr_sum)
-            curr.next = new_node
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
 
-            # update pointers
+            total = x + y + carry
+            carry = total // 10
+
+            curr.next = ListNode(total % 10)
             curr = curr.next
-            head_1 = head_1.next if head_1 else None
-            head_2 = head_2.next if head_2 else None
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
         return dummy.next
-
-
-
-        
-
-
