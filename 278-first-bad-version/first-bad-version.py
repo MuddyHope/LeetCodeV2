e169fcf2ca42b4b192616280877e788936f011dc
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-22 16:30:52 -0700

    Time: 43 ms (67.79%) | Memory: 19.2 MB (65.37%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -4,15 +4,13 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         
-
         # binary search
         l, r = 1, n
 
         while l < r:
             mid = (l+r)//2
-            print(f"l: {l}, mid: {mid}, r: {r}")
             if isBadVersion(mid):
                 r = mid
             elif not isBadVersion(mid):
-                l = mid + 1
+                l = mid +1
         return l
