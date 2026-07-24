1547b06effe3e9d13f3fc5c3f75466416140aeab
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-23 19:35:15 -0700

    Time: 6 ms (61.19%) | Memory: 21.3 MB (19.09%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,18 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-
-        # Boyer - Moore Algorithm
-
-        candidate = 0
-        count = 0
-
-        for each in nums:
-            if count == 0:
-                candidate = each
-            
-            if each == candidate:
-                count += 1
-            else:
-                count -= 1
-        return candidate
+        counter = Counter(nums)
+        n = len(nums)
+        
+        for num, count in counter.items():
+            if count > n /2:
+                return num
