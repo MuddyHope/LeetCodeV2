a4bc23c9051c461231307afb2e846f2401a34b6c
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-26 18:21:58 -0700

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.65%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0  # position where the next non-zero should go
-
+        i = 0
         for j in range(len(nums)):
             if nums[j] != 0:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
+        
