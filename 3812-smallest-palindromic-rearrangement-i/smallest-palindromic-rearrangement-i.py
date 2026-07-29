9453aef656256957e990a57ea18758dbaa61833f
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-29 02:07:39 -0700

    Time: 491 ms (15.63%) | Memory: 21.7 MB (16.07%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,38 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+        freq = {i: 0 for i in range(26)}
+        
+        for each in s:
+            freq[ord(each) - ord('a')] += 1
+        n = len(s)
+
+        res = [0] * n
+        # zzaaaabbbzz -> aazzbbbzzaa
+        # even will split on the most left and most right
+        # odd will be added in the middle
+        i, j = 0, n -1
+
+        for letter_ord, count in freq.items():
+            if count == 0:
+                continue
+
+            ch = chr(letter_ord + ord('a'))
+            pairs = count // 2
+
+            res[i:i+pairs] = [ch] * pairs
+            res[j-pairs+1:j+1] = [ch] * pairs
+
+            i += pairs
+            j -= pairs
+
+            if count % 2:
+                res[n // 2] = ch
+        
+        
+        return "".join(res)
+
+
+        # aabbbzzzaa -> 
+
