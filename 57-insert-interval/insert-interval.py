2dcd5ed151d70a27604a244efd0bbcdc2db10a22
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-29 15:22:27 -0700

    Time: 23 ms (5.24%) | Memory: 21.7 MB (7.08%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,24 +1,15 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
         intervals.append(newInterval)
-        intervals.sort()
+        intervals.sort(key= lambda x:  x[0])
+        stack = []
 
-        # [[1,3], [2,5], [6,9]]
-
-        res = [intervals[0]]
-        i = 1
-        while i < len(intervals):
-            if res[-1][1] >= intervals[i][0]:
-                res[-1][1] = max(intervals[i][1], res[-1][1])
+        for start, end in intervals:
+            print(f"curr: {start,end} stack : {stack}")
+            if stack and stack[-1][1] >= start:
+                _start, _end = stack.pop()
+                stack.append([_start, max(_end, end)])
             else:
-                res.append(intervals[i])
-            i += 1
-        return res
-
-
-
-
-
-        
-
-        
+                stack.append([start,end])
+        return stack
