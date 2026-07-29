7331b9bffa44d2a1852e9f45c33954d762ee54de
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-29 15:25:00 -0700

    Time: 0 ms (100.00%) | Memory: 21.5 MB (26.67%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,8 +1,19 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
-        intervals.append(newInterval)
-        intervals.sort(key= lambda x:  x[0])
+        left = 0
+        right = len(intervals) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if intervals[mid][0] < newInterval[0]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        # left is the insertion position
+        intervals.insert(left, newInterval)
         stack = []
 
         for start, end in intervals:
