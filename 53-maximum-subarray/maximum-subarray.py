eb55ed4309590c6f65f76061b1bd39c5347c8a59
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-29 17:15:53 -0700

    Time: 52 ms (11.14%) | Memory: 31.5 MB (19.61%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,23 +3,18 @@ class Solution:
         
         if len(nums) == 1:
             return nums[0]
-
-        max_res = nums[0]
-        i , j = 0, 1
-
-            
-        #   0 -2 -1 -5 -1 -2 0 1 -4 0
-        #  -3 -1 -2 5  1  2 0 -1 4 0
-        res_so_far = nums[0]
-        while j < len(nums):
-            print(f"i: {i}, j: {j}")
-            if res_so_far + nums[j] < nums[j]:
-                res_so_far = nums[j]
-                i = j
-            else:
-                res_so_far += nums[j]
-            max_res = max(res_so_far, max_res)
         
-            j += 1
+        l, r = 0, 1
+        max_res = nums[0]
+        
+        res_so_far = nums[0]
+        while r < len(nums):
+            if nums[r] + res_so_far < nums[r]:
+                res_so_far = nums[r]
+                l = r
+            else:
+                res_so_far += nums[r]
+            max_res = max(res_so_far, max_res)
+            r += 1
         return max_res
-
+        
