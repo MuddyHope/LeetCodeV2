7ac9f0e2f133a175072d828de6d3947700355103
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-29 17:54:38 -0700

    Time: 11 ms (16.78%) | Memory: 20.5 MB (23.54%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,10 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        i = 0
-        for j in range(len(nums)):
-            if nums[j] != 0:
-                nums[i],nums[j] = nums[j], nums[i]
+        # two-pointer
+
+        i, j = 0, 1
+
+        # 0 1 0 3 12
+
+        while j < len(nums):
+            if nums[i] == 0 and nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]     # swapped
                 i += 1
+            elif nums[i] != 0:
+                i += 1
+            j += 1
         
+            
