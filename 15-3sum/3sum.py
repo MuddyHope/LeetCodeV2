b21cdb2b2bf0b410f6c3dd3a92bf86fb4aac6687
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-30 21:02:44 -0700

    Time: 663 ms (33.09%) | Memory: 22.2 MB (53.37%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,29 +1,33 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
+        n = len(nums)
+        l, r = 0, n -1
         res = []
-        # [-4,-1,-1,0,1,2]
+        # [-1,0,1,2,-1,-4]
+        # -4 -1 -1 0 1 2
         i = 0
-        while i < len(nums) - 2:
-            j = i + 1
-            k = len(nums) - 1
-            if i > 0 and nums[i] == nums[i-1]:
-                i += 1
-                continue
-            while j < k:
-                # print(i, j, k)
-                curr_sum = nums[i] + nums[j] + nums[k]
-                if curr_sum == 0:
-                    res.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    k -= 1
-                    while j < k and nums[k] == nums[k+1]:
-                        k -=1
-                    while j < k and nums[j] == nums[j-1]:
-                        j += 1
+        while i < n-2:
+            # if nums[i] == nums[i+1]:
+            #     i += 1
+            #     continue
+            l = i + 1
+            r = n -1
+            seen = set()
+            
+            while l < r:
+                curr_sum = nums[i] + nums[l] + nums[r]
+                if curr_sum == 0 and (nums[i], nums[l], nums[r]) not in seen:
+                    res.append([nums[i], nums[l], nums[r]])
+                    seen.add((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
                 elif curr_sum > 0:
-                    k -= 1
+                    r -= 1
                 else:
-                    j += 1
+                    l += 1
             i += 1
+            while i < n and nums[i] == nums[i-1]:
+                i += 1
         return res
+
