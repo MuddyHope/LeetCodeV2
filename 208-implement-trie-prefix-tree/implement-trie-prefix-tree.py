866b7af6157267f6f5eb0bf2c0bc228368fcda4b
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-07-31 15:22:08 -0700

    Time: 346 ms (5.08%) | Memory: 25.9 MB (99.24%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,56 +1,20 @@
-class Node:
-    def __init__(self, val: str|None = None, next={}):
-        self.val = None
-        self.next = {}
-        self.is_end = False 
-
 class Trie:
 
     def __init__(self):
-        # create a rootNode with No value
-        self.root_node = Node("*", next={})
+        self.word = []
 
+    def insert(self, word: str) -> None:
+        self.word.append(word)
         
-    def insert(self, word: str) -> None:            
-        # start with insert new word only
-        # print(f"inserting: {word}")
-        curr = self.root_node
-        for letter in word:
-            if letter not in curr.next.keys():
-                curr.next[letter] = Node(letter)
-            # print(f"curr.next is {curr.next}")
-            curr = curr.next[letter]
-        curr.is_end = True
-
-
     def search(self, word: str) -> bool:
-        curr = self.root_node
-        i = 0
-        # print(f"searching word: {word}")
-        while curr and i <= len(word) - 1:
-            if word[i] in curr.next:
-                # print(f"i: {i}, letter: {word[i]} present in curr.next: {curr.next}")
-                curr = curr.next[word[i]]
-                i += 1
-            else:
-                # print(f"not found, i: {i}, letter: {word[i]} in curr.next: {curr.next}")
-                return False
-        return curr.is_end
-
-            
+        return True if word in self.word else False
 
     def startsWith(self, prefix: str) -> bool:
-        curr = self.root_node
-        i = 0
-        # print(f"prefix : {prefix}")
-        while i <= len(prefix) - 1:
-            if prefix[i] in curr.next:
-                # print(f"startswith i: {i}, letter: {prefix[i]} present in curr.next: {curr.next}")
-                curr = curr.next[prefix[i]]
-                i += 1
-            else:
-                return False
-        return True
+        for each in self.word:
+            if each.startswith(prefix):
+                return True
+        return False
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
