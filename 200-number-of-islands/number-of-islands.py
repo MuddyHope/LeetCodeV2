6b6aa058afb72161f0f16bd5f0784d129847dbe8
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-01 20:47:02 -0700

    Time: 371 ms (5.13%) | Memory: 39.5 MB (6.86%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,35 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # start from any 1, keep track of all the seen
-
-        directions = [(0,1), (1,0), (0,-1), (-1,0)]
-
-        res = 0
-        rows_max = len(grid)
-        cols_max = len(grid[0])
-        seen = set()
-
-        def dfs(row, col, res):
-            if not (0 <= row < rows_max and 0 <= col < cols_max):
-                return
         
-            if (row, col) in seen:
+        res = 0
+        max_rows = len(grid)
+        max_cols = len(grid[0])
+        seen = set()
+        def dfs(r, c):
+            nonlocal res
+            if not (0 <= r < max_rows) or not (0 <= c < max_cols):
                 return
-            
-            if grid[row][col] == "0":
+
+            if (r,c) in seen:
                 return
-            
-            seen.add((row,col))
+            print(f"curr: r, c: {r,c} {grid[r][c]}")
 
-            for dx,dy in directions:
-                nx, ny = row+dx, col+dy
-                dfs(nx, ny, res)
-            return
+            seen.add((r,c))
+            if grid[r][c] == "0":
+                return False
             
+            for dx, dy in (0,1), (1,0), (0,-1), (-1,0):
+                nx, ny = r+ dx, c + dy
+                dfs(nx, ny)
 
-        for row in range(rows_max):
-            for col in range(cols_max):
-                if (row,col) not in seen and grid[row][col] == "1":
+
+        for r in range(max_rows):
+            for c in range(max_cols):
+                if grid[r][c] == "1" and (r,c) not in seen:
+                    dfs(r,c)
                     res += 1
-                    dfs(row,col, res)
         return res
