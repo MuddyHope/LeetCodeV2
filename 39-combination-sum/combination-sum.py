a33db2003c141514f532817e5a617ab7386a91f6
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-03 20:26:27 -0700

    Time: 11 ms (47.93%) | Memory: 19.6 MB (32.66%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -4,31 +4,19 @@ class Solution:
         res = []
         nums = candidates
 
-        seen = set()
-
         def dfs(i, curr_sum, temp):
-            # print(f"i: {i}, curr_sum: {curr_sum}, temp: {temp}")
-            if i >= len(candidates) or curr_sum > target:
+            if curr_sum > target:
                 return
-
-            if curr_sum == target and tuple(temp) not in seen:
-                # print(f"adding to temp")
-                res.append(temp[:])
-                seen.add(tuple(temp))
-        
-            curr_sum += nums[i]
-            temp.append(nums[i])
-
-            for j in range(i, len(nums)):
-                # print(f"j: {j}, curr_sum: {curr_sum}, temp: {temp}")
-                dfs(j, curr_sum, temp)
-            curr_sum -= nums[j]
-            temp.pop()
-            return
             
-
-        for i in range(len(candidates)):
-            print(f"I from for loop: {i}")
-            dfs(i, 0, [])
-        return res
+            if curr_sum == target:
+                res.append(temp[:])
+                return
+            
+            for j in range(i, len(nums)):
+                temp.append(nums[j])
+                dfs(j, curr_sum + nums[j], temp)
+                temp.pop()
         
+
+        dfs(0, 0, [])
+        return res
