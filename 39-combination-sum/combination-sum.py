199e20e02e4cae123250fcfe07c971c2c39a0e60
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-03 20:08:28 -0700

    Time: 42 ms (5.21%) | Memory: 19.8 MB (8.95%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,34 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        res, sol = [], []
+        res = []
+        nums = candidates
 
-        def dfs(i, curr_sum):
-            if curr_sum == target:
-                res.append(sol[:])
+        seen = set()
+
+        def dfs(i, curr_sum, temp):
+            # print(f"i: {i}, curr_sum: {curr_sum}, temp: {temp}")
+            if i >= len(candidates) or curr_sum > target:
                 return
-            elif curr_sum > target or i == len(candidates):
-                return
-            
-            dfs(i+1, curr_sum)
-            sol.append(candidates[i])
-            dfs(i, curr_sum + candidates[i])
-            sol.pop()
+
+            if curr_sum == target and tuple(temp) not in seen:
+                # print(f"adding to temp")
+                res.append(temp[:])
+                seen.add(tuple(temp))
         
-        dfs(0, 0)
+            curr_sum += nums[i]
+            temp.append(nums[i])
+
+            for j in range(i, len(nums)):
+                # print(f"j: {j}, curr_sum: {curr_sum}, temp: {temp}")
+                dfs(j, curr_sum, temp)
+            curr_sum -= nums[j]
+            temp.pop()
+            return
+            
+
+        for i in range(len(candidates)):
+            print(f"I from for loop: {i}")
+            dfs(i, 0, [])
         return res
+        
