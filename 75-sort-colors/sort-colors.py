69d0424cf5fe3629bada05cd4b4069c155f246c0
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-06 01:59:45 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.35%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,36 +1,11 @@
+from collections import Counter
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        count0 = 0
-        count1 = 0
-        count2 = 0
+        counter = Counter(nums)
 
-        # First pass: count frequencies
-        for num in nums:
-            if num == 0:
-                count0 += 1
-            elif num == 1:
-                count1 += 1
-            else:
-                count2 += 1
-
-        # Second pass: overwrite array
-        i = 0
-
-        while count0 > 0:
-            nums[i] = 0
-            i += 1
-            count0 -= 1
-
-        while count1 > 0:
-            nums[i] = 1
-            i += 1
-            count1 -= 1
-
-        while count2 > 0:
-            nums[i] = 2
-            i += 1
-            count2 -= 1
-        
+        j = 0
+        for i in range(3):
+            count = counter.get(i, 0)
+            nums[j:j+count] = [i] * count
+            j += count
