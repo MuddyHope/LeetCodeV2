b8db95ebf0e6986e137e4fc10df1f226264add3f
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-06 16:53:33 -0700

    Time: 29 ms (51.48%) | Memory: 25 MB (20.28%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,42 +1,32 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        email_to_name = defaultdict(list)
+        email_to_name = {}
         graph = defaultdict(set)
-        for each in accounts:
-            name = each[0]
-            for each_email in each[1:]:
-                graph[each_email].add(each[1])
-                graph[each[1]].add(each_email)
-                email_to_name[each_email] = name
 
-        # print(dict(email_to_name))
-        # print(graph)
-
-        visited = set()
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                graph[email].add(account[1])
+                graph[account[1]].add(email)
+                email_to_name[email] = name
+        
+        seen = set()
         stack = []
         res = []
 
         for email in graph:
-            if email not in visited:
+            if email not in seen:
+                seen.add(email)
                 stack = [email]
-                visited.add(email)
-                local_res = []
 
+                local_res = []
                 while stack:
                     curr = stack.pop()
                     local_res.append(curr)
 
                     for edge in graph[curr]:
-                        if edge not in visited: 
-                            visited.add(edge)
+                        if edge not in seen:
+                            seen.add(edge)
                             stack.append(edge)
-                                           
                 res.append([email_to_name[email]] + sorted(local_res))
         return res
-
-
-
-
-
-            
-
