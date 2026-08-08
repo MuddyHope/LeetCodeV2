72f9cb4cc6b5e21bcd9284c683aaf9cbba8fd8a7
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-07 17:29:24 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (93.31%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,33 +6,27 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+
         res = []
-        hash_map = defaultdict(list)
-        max_level = 0
-        # dfs 
-        def dfs(node, level):
-            nonlocal max_level
-            if not node:
-                return None, level
+        dq = deque([root])
 
-            right = dfs(node.right, level+1)
-            left = dfs(node.left, level+1)
-
-            max_level = max(level+1, max_level)
-
-            if not hash_map[level]:
-                hash_map[level].append(node.val)
-            
-            return (right or left, level)
-
-
-        dfs(root, 0)
-    
-        for i in range(max_level):
-            res.append(hash_map[i][0])
+        while dq:
+            # print(f"stack: {dq}")
+            temp = []
+            for i in range(len(dq)):
+                curr = dq.popleft()
+                if not curr:
+                    continue
+                temp.append(curr.val)
+                # print(f"curr_val: {curr.val}")
+                if curr.right:
+                    dq.append(curr.right)
+                if curr.left:
+                    dq.append(curr.left)
+            if temp:
+                res.append(temp[0])
         
         return res
-        
-        
-
+            
 
