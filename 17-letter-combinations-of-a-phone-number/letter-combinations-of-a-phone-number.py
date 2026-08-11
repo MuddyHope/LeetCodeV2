1472a4a40a537ec749fef406964345e6f1814c9c
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-11 15:36:24 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.43%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,28 +1,32 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
-        number_dict = {2: "abc", 3:"def", 4:"ghi", 5:"jkl", 6:"mno", 7:"pqrs", 8:"tuv", 9:"wxyz"}
+        hash_map = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz"
+        }
+
         res = []
+
+        def dfs(i, temp):
+            print(f"i: {i}, temp: {temp}")
+
+            if len(temp) == len(digits):
+                res.append("".join(temp))
+                return
+
+            for letter in hash_map[int(digits[i])]:
+                temp.append(letter)
+                dfs(i + 1, temp)
+                temp.pop()
+
         if not digits:
-            return res
+            return []
 
-        subset = ""
-        def dfs(i):
-            nonlocal subset
-            # print(f"i: {i}, subset: {subset}")
-            if len(subset) == len(digits):
-                res.append(subset)
-                return
-            if i > len(digits):
-                return
-
-            for each in number_dict[int(digits[i])]:
-                subset += each
-                # print(f"before dfs: {subset}")
-                dfs(i+1)
-                # print(f"after dfs: {subset}")
-                subset = subset[:len(subset)-1]
-        
-        dfs(0)
-
+        dfs(0, [])
         return res
