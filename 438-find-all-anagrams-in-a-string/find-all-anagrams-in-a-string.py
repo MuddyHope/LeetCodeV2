2088b69aea588f0b9b5141fe590be9ac9557c179
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-11 18:10:40 -0700

    Time: 48 ms (45.43%) | Memory: 19.9 MB (15.63%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,35 +1,21 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        len_s = len(s)
-        len_p = len(p)
-
-        if len_s < len_p:
-            return []
-        
-        p_count = {}
-        for i in p:
-            p_count[i] = 1 + p_count.get(i, 0)
-        window_count = {}
-        for j in s[:len_p]:
-            window_count[j] = 1 + window_count.get(j, 0)
-
         res = []
-        if p_count == window_count:
-            res.append(0)
 
-        for each in range(len_p, len_s):
-            first_letter = s[each - len_p]
-            last_letter = s[each]
+        p_counter = dict(Counter(p))
 
-            # Decrement the count of the letter going out of the window
-            window_count[first_letter] -= 1
-            if window_count[first_letter] == 0:
-                del window_count[first_letter]
+        l = 0
+        c_dict = {}
+        for r in range(len(s)):
+            c_dict[s[r]] = 1 + c_dict.get(s[r], 0)
+            # print(f"l,r: {l, r}, c_dict: {c_dict}")
 
-            # Increment the count of the new letter in the window
-            window_count[last_letter] = 1 + window_count.get(last_letter, 0)
-
-            if p_count == window_count:
-                res.append(each - len_p + 1)
-
+            if c_dict == p_counter:
+                res.append(l)
+                
+            while r-l+1 >= len(p):
+                c_dict[s[l]] = c_dict[s[l]] - 1
+                if c_dict[s[l]] == 0:
+                    del c_dict[s[l]]
+                l += 1
         return res
