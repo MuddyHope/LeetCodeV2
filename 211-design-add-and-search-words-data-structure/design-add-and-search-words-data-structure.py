47cc7a9d8f1e4b485737480f059859acc197ff83
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-13 18:08:23 -0700

    Time: 899 ms (80.45%) | Memory: 68.3 MB (61.52%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,55 +1,47 @@
-class Node:
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
 
-    def __init__(self, val, next={}):
-        self.val = val
-        self.next = {}
-        self.is_end = False
 
 class WordDictionary:
 
     def __init__(self):
-        # intitalise the root
-        self.root = Node("*", next={})
-        
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
-        i = 0
         curr = self.root
-        my_set = set()
-        while i<len(word) and curr:
-            if word[i] not in curr.next:
-                curr.next[word[i]] = Node(word[i])
-            curr = curr.next[word[i]]
-            i += 1
-        curr.is_end = True
 
+        for letter in word:
+            if letter not in curr.children:
+                curr.children[letter] = TrieNode()
+
+            curr = curr.children[letter]
+
+        curr.isWord = True
 
     def search(self, word: str) -> bool:
-        def backtrack(node, i):
 
-            # ✅ if we've consumed the word, check if this node is an end
+        def dfs(node, i):
+
+            # We consumed the entire search word
             if i == len(word):
-                return node.is_end
+                return node.isWord
 
-            ch = word[i]
+            letter = word[i]
 
-            # ✅ handle wildcard
-            if ch == ".":
-                for child in node.next.values():
-                    if backtrack(child, i + 1):
-                        return True
-                return False
-            else:
-                if ch not in node.next:
+            # Normal character
+            if letter != ".":
+                if letter not in node.children:
                     return False
-                return backtrack(node.next[ch], i + 1)
 
-        return backtrack(self.root, 0)
+                return dfs(node.children[letter], i + 1)
 
+            # "." means any character
+            for child in node.children.values():
+                if dfs(child, i + 1):
+                    return True
 
+            return False
 
-
-# Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
+        return dfs(self.root, 0)
