c05df674c13a02bca1839e4b5d223ed99144a347
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-17 00:40:12 -0700

    Time: 3 ms (86.08%) | Memory: 20.2 MB (92.95%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,29 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+
         adj_list = defaultdict(list)
-        for course, prereq in prerequisites:
-            adj_list[course].append(prereq)
-        
+        indegree = [0] * numCourses
 
-        seen = set()
+        # Build graph + count prerequisites
+        for crs, pre in prerequisites:
+            adj_list[pre].append(crs)
+            indegree[crs] += 1
 
-        def dfs(course):
-            if not adj_list[course]:
-                return True
-            
-            if course in seen:
-                return False
-            
-            seen.add(course)
+        # Courses with no prerequisites
+        dq = deque()
 
-            for pre in adj_list[course]:
-                if not dfs(pre) : return False
-            
-            adj_list[course] = []
-            return True
+        for crs in range(numCourses):
+            if indegree[crs] == 0:
+                dq.append(crs)
 
-        for course in range(numCourses):
-            if not dfs(course):
-                return False
-        return True
+        completed = 0
+
+        while dq:
+            curr = dq.popleft()
+            completed += 1
+
+            for crs in adj_list[curr]:
+                indegree[crs] -= 1
+
+                if indegree[crs] == 0:
+                    dq.append(crs)
+
+        return completed == numCourses
