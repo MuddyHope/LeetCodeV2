3b7e4fa33448927e286c85070123331002834990
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-17 21:36:14 -0700

    Time: 22 ms (5.60%) | Memory: 20.3 MB (36.78%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -1,24 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         
         res = []
 
         def backtrack(node, curr_sum, sol):
+            
             if not node:
                 return
-
-            sol.append(node.val)
+            
             curr_sum += node.val
+            sol.append(node.val)
+            print(f"curr: {node.val}, curr{curr_sum}, sol: {sol}")
 
-            if curr_sum == targetSum and not node.left and not node.right:
+            if curr_sum == targetSum and not node.right and not node.left:
                 res.append(sol[:])
                 sol.pop()
                 return
-
-            backtrack(node.left, curr_sum, sol)
-            backtrack(node.right, curr_sum, sol)
-
+            
+            if node.left:
+                backtrack(node.left, curr_sum, sol)
+            if node.right:
+                backtrack(node.right, curr_sum, sol)
             sol.pop()
+        
+
 
         backtrack(root, 0, [])
         return res
