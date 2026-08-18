2bf310f1b341658d85c34bbc5a4b3760efd949e3
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-18 02:25:00 -0700

    Time: 363 ms (2.66%) | Memory: 20.1 MB (57.00%) - LeetSync

---
+++ 112-path-sum/path-sum.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        res = False
+        def backtrack(node, curr_sum):
+            nonlocal res
+            if not node:
+                return True
+            
+            curr_sum += node.val
+            print(f"curr: {node}")
+
+            if curr_sum == targetSum and not node.left and not node.right:
+                res = True
+                return
+
+            backtrack(node.left, curr_sum)
+            backtrack(node.right, curr_sum)
+        backtrack(root, 0)
+        return res
+            
