5792cfa217e3d8faefcdace01ee7b31c145550be
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-19 18:55:13 -0700

    Time: 5 ms (32.23%) | Memory: 19.4 MB (10.89%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -6,19 +6,31 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
+        dummy = ListNode(0)
+        head = dummy
+        carry = 0
 
-        cout = 0
-        dummy = head = ListNode(0)
-
-        while l1 or l2 or cout:
-            x = l1.val if l1 else 0
-            y = l2.val if l2 else 0
-            res = x + y + cout
-            cout = res // 10
-            head.next = ListNode(res % 10)
+        while l1 or l2 or carry:
+            curr_sum = carry
             if l1:
+                curr_sum += l1.val
                 l1 = l1.next
             if l2:
+                curr_sum += l2.val
                 l2 = l2.next
+
+            if curr_sum >= 10:
+                carry = 1
+            else:
+                carry = 0
+            node = ListNode(curr_sum%10)
+            head.next = node
             head = head.next
+               
+        
+        if l1:
+            head.next = l1
+        
+        if l2:
+            head.next =l2
         return dummy.next
