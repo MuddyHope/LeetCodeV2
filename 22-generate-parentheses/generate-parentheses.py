478afe3953b4479a45788edc3a6eb7c77bbc43ba
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-21 00:00:25 -0700

    Time: 4 ms (12.48%) | Memory: 19.3 MB (73.26%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,7 +2,7 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
         def dfs(sol, open, close):
-            print(f"sol: {sol}, open: {open}, close: {close}")
+            # print(f"sol: {sol}, open: {open}, close: {close}")
             if open == n and close == n:
                 res.append("".join(sol))
                 return
