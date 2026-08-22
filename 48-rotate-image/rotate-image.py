cf28f2c7ca5a4dc3049b86c574827aae30aeae25
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-22 15:24:29 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.73%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -13,21 +13,21 @@ class Solution:
         while top < bottom and left < right:
 
             for i in range(left, right):
-                print(f"i: {i}, left: {left}, right: {right}, top: {top}, bottom: {bottom}")
+                # print(f"i: {i}, left: {left}, right: {right}, top: {top}, bottom: {bottom}")
 
                 left_top = temp = matrix[top][i]
                 right_top = matrix[i][right]
                 bottom_right = matrix[bottom][right - (i - left)]
                 bottom_left = matrix[bottom - (i - left)][left]
 
-                print(left_top, right_top, bottom_right, bottom_left)
+                # print(left_top, right_top, bottom_right, bottom_left)
 
                 matrix[bottom - (i - left)][left] = bottom_right
                 matrix[top][i] = bottom_left
                 matrix[i][right] = left_top
                 matrix[bottom][right - (i - left)] = right_top
 
-                print(f"matrix: {matrix}")
+                # print(f"matrix: {matrix}")
 
             top += 1
             left += 1
