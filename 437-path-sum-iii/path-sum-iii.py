fa5902719bb55563818b04de42f7aa8ea7ad4e32
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-24 18:25:03 -0700

    Time: 115 ms (35.03%) | Memory: 19.9 MB (69.30%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        res = 0
+
+        def dfs(node, vals):
+            nonlocal res
+
+            if not node:
+                return
+
+            vals.append(node.val)
+
+            # Check every path ending at the current node
+            curr_sum = 0
+
+            for i in range(len(vals) - 1, -1, -1):
+                curr_sum += vals[i]
+
+                if curr_sum == targetSum:
+                    res += 1
+
+            dfs(node.left, vals)
+            dfs(node.right, vals)
+
+            # Backtrack
+            vals.pop()
+
+        dfs(root, [])
+
+        return res
