195dfb36da85f71a8d3529255ada9fa615212f00
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-26 22:39:00 -0700

    Time: 3 ms (50.04%) | Memory: 28.1 MB (6.89%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,41 +5,41 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-
         """
         Do not return anything, modify head in-place instead.
         """
-        # find mid
-        slow, fast = head, head.next
-        while fast and fast.next:
+
+        if not head or not head.next:
+            return 
+        
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None
 
-        # mid is slow.next
+        prev = None
 
-        # now reverse the second half
-        second = slow.next  # 3
-        prev = slow.next = None # None
         while second:
-            temp = second.next # 4 # 5
-            second.next = prev  # 3 -> None # 4 -> 3 -> None
-            prev = second # 3   # 4
-            second = temp   # 4 # 5
-        # print_list(prev)
-        # [5] -> [4] -> None
+            nxt = second.next
+            second.next = prev
+            prev = second
+            second = nxt
+        
+        second = prev
+        first = head
 
-        # print_list(head)
-        # [1] -> [2] -> [3] -> None
-
-        # now we have to merge it
-        first, second = head, prev
         while second:
-            temp1, temp2 = first.next, second.next  # 2, 4 # None, 3
-            first.next = second # 1->5  # 2->4
-            second.next = temp1 # 1->5->2   #4->None
-            first, second  = temp1, temp2   # 2, 4 # None, 3
+            temp1 = first.next
+            temp2 = second.next
 
+            first.next = second
+            second.next = temp1
 
-
-
-
+            first = temp1
+            second = temp2
+        
