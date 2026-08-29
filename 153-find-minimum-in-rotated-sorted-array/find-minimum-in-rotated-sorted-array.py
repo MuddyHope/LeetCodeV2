442f8731953bf27779597df6119d5b4813b461c1
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-28 19:42:00 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (93.08%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,13 +1,6 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # find max, then go next step
-        l, r = 0, len(nums)-1
-        min_so_far = float("+inf")
-        while l<=r:
-            mid = (l+r)//2
-            min_so_far = min(min_so_far, nums[mid])
-            if nums[r] < nums[mid]:
-                l = mid+1
-            else:
-                r = mid - 1
-        return min_so_far
+        
+        # binary search to find the tipping point
+
+        return min(nums)
