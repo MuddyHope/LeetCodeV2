c7d389374dad9d2b0af9dfa05903728b43ad96da
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-08-28 20:06:52 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (66.95%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -3,4 +3,14 @@ class Solution:
         
         # binary search to find the tipping point
 
-        return min(nums)
+        l , r = 0, len(nums) -1
+
+        while l < r:
+            mid = (l+r) //2
+            print(f"l: {l}, mid: {mid}, r: {r}")
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
+            
