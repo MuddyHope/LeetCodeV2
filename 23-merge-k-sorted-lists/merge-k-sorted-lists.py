fe713d08bb41f0d31098463712d24455a50f5906
MuddyHope <60110059+MuddyHope@users.noreply.github.com>
2026-09-12 18:27:36 -0700

    Time: 3 ms (99.10%) | Memory: 23 MB (47.77%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,37 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
-            return None
+        final_list = []
 
-        while len(lists) > 1:
-            merged_lists = []
-
-            for i in range(0, len(lists), 2):
-                print(i)
-                l1 = lists[i]
-                l2 = lists[i+1] if i + 1 < len(lists) else None
-            
-                merged_lists.append(self.mergeLists(l1, l2))
-            lists = merged_lists
-        return lists[0]
-
-    def mergeLists(self, l1, l2):
-        new_head = ListNode()
-
-        curr = new_head
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
-            curr = curr.next
+        for list in lists:
+            head = list
+            while head:
+                final_list.append(head.val)
+                head = head.next
         
-        if l1:
-            curr.next = l1
-        if l2:
-            curr.next = l2
-        return new_head.next
+        # print(final_list)
+        final_list.sort()
+        
+        dummy = ListNode(0)
+        head = dummy
+        for i in final_list:
+            head.next = ListNode(i)
+            head = head.next
+        
+        return dummy.next
+
